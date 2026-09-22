dfbe34573bccd5648a6bf8cd74b9ecea498f5c96
PIDOWU-24 <pidowu-24@Mac.lan>
2026-09-21 22:15:42 -0400

    Add argument count check

--- find_homologs.sh
+++ find_homologs.sh
@@ -6,11 +6,3 @@ if [ "$#" -ne 3 ]; then
     exit 1
 fi
 
-tblastn -query "$1" \
-  -subject "$2" \
-  -outfmt "6 qseqid sseqid pident qlen length" \
-  > raw_hits.tsv
-
-awk '$3 > 30 && $5 > 0.9 * $4' raw_hits.tsv > "$3"
-
-wc -l < "$3"
