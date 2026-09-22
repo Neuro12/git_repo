ec7d01c36c5b228e1c1efbbcdca4258bc5155eda
PIDOWU-24 <pidowu-24@Mac.lan>
2026-09-21 22:59:25 -0400

    Fix stray character in output redirect

--- find_homologs.sh
+++ find_homologs.sh
@@ -11,6 +11,6 @@ tblastn -query "$1" \
   -outfmt "6 qseqid sseqid pident qlen length" \
   > raw_hits.tsv
 
-awk '$3 > 30 && $5 > 0.9 * $4' raw_hits.tsv > "$3"≈
+awk '$3 > 30 && $5 > 0.9 * $4' raw_hits.tsv > "$3"
 
 wc -l < "$3"
